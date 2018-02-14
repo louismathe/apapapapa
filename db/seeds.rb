fa6fadf134bb6fe9375de88f91e397714e0ba516
Louis Mathe <mathe.louis@gmail.com>
2018-02-14 14:52:08 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Review.destroy_all
+Book.destroy_all
+Author.destroy_all
+
+
+puts "Creating Author"
+# Author : Name + biography
+author = Author.create!(name: 'Toto', biography: "jiaezjioaze oijazeoija oiiajze ouhazi")
+
+puts "Creating Book"
+# Book : Title + Summary + Author_id
+book = Book.create!(title: 'ijijijij', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja', author: author)
+book1 = Book.create!(title: 'Book1', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja', author: author)
+
+puts "Creating Reviews"
+# Review : Subject + Content + Book_id
+first_review = Review.create!(subject: 'Pas Cool', content: 'blalblbllblblbl', book: book)
+second_review = Review.create!(subject: 'Génial', content: 'ziazjeiaziazizaiaziaz', book: book)
