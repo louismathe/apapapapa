9ae06aba3090cf5482d4e9ae0cb9b95027788071
Louis Mathe <mathe.louis@gmail.com>
2018-02-15 22:32:24 +0100

    add user ref to com & review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215210323) do
+ActiveRecord::Schema.define(version: 20180215212455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,9 @@ ActiveRecord::Schema.define(version: 20180215210323) do
     t.bigint "review_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["review_id"], name: "index_comments_on_review_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -55,7 +57,9 @@ ActiveRecord::Schema.define(version: 20180215210323) do
     t.bigint "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,7 +71,9 @@ ActiveRecord::Schema.define(version: 20180215210323) do
 
   add_foreign_key "books", "authors"
   add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "comments"
   add_foreign_key "likes", "users"
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Like.destroy_all
-User.destroy_all
-
 Comment.destroy_all
 Review.destroy_all
+User.destroy_all
+
 Book.destroy_all
 Author.destroy_all
 
@@ -20,21 +20,22 @@ Book.create!(title: 'Book1', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja'
 puts 'Creating users'
 user = User.new(name:'bob')
 user2 = User.new(name:'georges')
-
+user3 = User.new(name: 'paul')
+user4 = User.new(name: 'John')
 
 
 puts "Creating Reviews"
 # Review : Subject + Content + Book_id
-review = Review.create!(subject: 'Cool', content: 'blalblbllblblbl', book: book)
-Review.create!(subject: 'trolololo', content: 'ziazjeiaziazizaiaziaz', book: book)
+review = Review.create!(subject: 'Cool', content: 'blalblbllblblbl', book: book, user: user3)
+Review.create!(subject: 'trolololo', content: 'ziazjeiaziazizaiaziaz', book: book, user: user4)
 
 
 puts "Creating comments"
 
-com = Comment.create!(content:'I don\'t care', review: review)
-Comment.create!(content:'I agree', review: review)
-Comment.create!(content:'I disagree', review: review)
-Comment.create!(content:'I do not agree', review: review)
+com = Comment.create!(content:'I don\'t care', review: review, user: user4)
+Comment.create!(content:'I agree', review: review, user: user)
+Comment.create!(content:'I disagree', review: review, user: user2)
+Comment.create!(content:'I do not agree', review: review, user: user3)
 
 
 
