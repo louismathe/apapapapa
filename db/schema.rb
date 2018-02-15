c2ba61a551d43ec4fc2b22b3b7d5192380465505
Louis Mathe <mathe.louis@gmail.com>
2018-02-15 21:54:34 +0100

    comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214125610) do
+ActiveRecord::Schema.define(version: 20180215204412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20180214125610) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.string "subject"
     t.text "content"
@@ -41,5 +49,6 @@ ActiveRecord::Schema.define(version: 20180214125610) do
   end
 
   add_foreign_key "books", "authors"
+  add_foreign_key "comments", "reviews"
   add_foreign_key "reviews", "books"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,18 @@ author = Author.create!(name: 'Toto', biography: "jiaezjioaze oijazeoija oiiajze
 puts "Creating Book"
 # Book : Title + Summary + Author_id
 book = Book.create!(title: 'ijijijij', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja', author: author)
-book1 = Book.create!(title: 'Book1', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja', author: author)
+Book.create!(title: 'Book1', summary: 'izaejiazjeiaezjiazejazijeiazjiazejazieja', author: author)
 
 puts "Creating Reviews"
 # Review : Subject + Content + Book_id
-first_review = Review.create!(subject: 'Pas Cool', content: 'blalblbllblblbl', book: book)
-second_review = Review.create!(subject: 'Génial', content: 'ziazjeiaziazizaiaziaz', book: book)
+review = Review.create!(subject: 'Cool', content: 'blalblbllblblbl', book: book)
+Review.create!(subject: 'trolololo', content: 'ziazjeiaziazizaiaziaz', book: book)
+
+puts "Creating comments"
+
+com = Comment.create!(content:'I don\'t care', review: review)
+Comment.create!(content:'I agree', review: review)
+Comment.create!(content:'I disagree', review: review)
+Comment.create!(content:'I do not agree', review: review)
+
+# &1  Rest  N;Rev ::: rest:ref
